d0ccb24dde38e02018a9a1af1b094a50752361f8
nasibehteimouri <nasibehteimouri@gmail.com>
2018-02-12 11:35:58 -0500

    authorization interface -- pylint, pep8, and typo checked!

--- m2/interface/authorization.py
+++ m2/interface/authorization.py
@@ -6,13 +6,13 @@ class Authorization:
     __metaclass__ = ABCMeta
 
     @abstractmethod
-    def autherize_access_node(self, node_id):
-        """Autherize entity's access to a specific node"""
+    def authorize_access_node(self, node_id):
+        """Authorize entity's access to a specific node"""
 
     @abstractmethod
-    def autherize_access_image(self, image_id):
-        """Autherize entity's access to a specific image"""
+    def authorize_access_image(self, image_id):
+        """Authorize entity's access to a specific image"""
 
     @abstractmethod
-    def autherize_access_tag(self, tag_id):
-        """Autherize entity's access to a specific tag"""
+    def authorize_access_tag(self, tag_id):
+        """Authorize entity's access to a specific tag"""
