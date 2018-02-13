fb5af3f8575eff0e514b9e856b5fa0d4f7fd817e
nasibehteimouri <nasibehteimouri@gmail.com>
2018-02-13 15:25:27 -0500

    authorization methods to authorize access to the resources outside of the M2 core

--- m2/interface/authorization.py
+++ m2/interface/authorization.py
@@ -1,56 +1,36 @@
-"""Auhtorization Interface"""
+""" Authorization Interface """
 from abc import ABCMeta, abstractmethod
 
 
 class Authorization:
-    """Specify access rights/privileges to resources"""
+    """ Specify access rights/privileges to resources """
     __metaclass__ = ABCMeta
 
     @abstractmethod
     def check_entity_node_access(self, entity_id, node_mac_addres):
-        """Authorize entity's access to reserve a specific node
+        """
+        Authorize entity's access to reserve a specific node
 
         param: entity_id: the id of the enetity
         param: node_mac_addres: the mac address of the node
-        return: success or failure
+        return: None in case of success, or otherwise raise an exception
         """
 
-    def check_entity_instance_access(self, entity_id, instance_id):
-        """Authorize entity's access to a provisioned instance
-
-        param: entity_id: the id of the enetity
-        param: instance_id: the id of the provisioned instance
-        return: success or failure
+    def check_entity_storage_access(self, entity_id, rbd_name):
         """
-
-    def check_entity_storage_access(self, entity_id, storage_id):
-        """Authorize entity's access to read/write from/to a storage
-
-        param: entity_id: the id of the enetity
-        param: storage_id: the id of the storage
-        return: success or failure
-        """
-
-    def check_entity_image_access(self, entity_id, instance_id):
-        """Authorize entity's access to an image
+        Authorize entity's access to read/write from/to a
+        specific storage.
 
         param: entity_id: the id of the enetity
-        param: image_id: the id of the image
-        return: success or failure
-        """
-
-    def check_entity_tag_access(self, entity_id, tag_id):
-        """Authorize entity's access to a tag
-
-        param: entity_id: the id of the enetity
-        param: tag_id: the id of the tag
-        return: success or failure
+        param: rbd_name: the name of the storage
+        return: None in case of success, or otherwise raise an exception
         """
 
     def check_entity_network_access(self, entity_id, network_id):
-        """Authorize entity's access to a network
+        """
+        Authorize entity's access to a specific NIC
 
         param: entity_id: the id of the enetity
-        param: network_id: the id of the tag
-        return: success or failure
+        param: network_id: the NIC used in provisioning
+        return: None in case of success, or otherwise raise an exception
         """
