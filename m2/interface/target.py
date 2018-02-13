7f0eb8c25409f5c624a010088deb4fe65414bd3c
nasibehteimouri <nasibehteimouri@gmail.com>
2018-02-13 16:22:48 -0500

    changed target based on the M2 core

--- m2/interface/target.py
+++ m2/interface/target.py
@@ -1,32 +1,32 @@
-"""Target Interface"""
+""" Target Interface """
 from abc import ABCMeta, abstractmethod
 
 
-class Authorization:
-    """Specify provisioning target"""
+class Target:
+    """ Specify provisioning target """
     __metaclass__ = ABCMeta
 
     @abstractmethod
-    def add_target(self, clone_name):
-        """Registers a new target with given image
-	param: clone_name: 
-        return: 
+    def add_target(self, instance_id, image_id):
+        """
+        Create a provisioning target to provision an instance using an image
+	param: instance_id: the id of the provisioned instance
+        param: image_id: the id of the image 
+        return: target_id: the id of the provisioning target 
         """
 
     @abstractmethod
-    def remove_target(self, clone_ceph_name):
-        """Remove a target
-        
+    def delete_target(self, instance_id):
+        """
+        Remove a target
+	param: instance_id: the id of the provisioned instance 
+        return: none in case of success, or otherwise raise an exception 
         """
 
     @abstractmethod
-    def list_targets(self):
-        """Show list of targets
-
+    def get_target_id(self, instance_id):
         """
-
-    @abstractmethod
-    def get_target_id(self):
-        """Show list of targets
-
+        Get the provisioning target id used for provisioning
+	param: instance_id: the id of the provisioned instance
+        return: target_id: the id of the provisioning target 
         """
