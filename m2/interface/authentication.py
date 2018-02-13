e6540eab9f1fbdb0e595872f750d5467465cebb7
nasibehteimouri <nasibehteimouri@gmail.com>
2018-02-13 12:52:01 -0500

    one more mthod, one chnaged method, more comments

--- m2/interface/authentication.py
+++ m2/interface/authentication.py
@@ -7,8 +7,21 @@ class Authentication:
     __metaclass__ = ABCMeta
 
     @abstractmethod
-    def is_legit(self, entity_id):
-        """Verify entity's legitimacy
+    def is_legit(self, user_name, user_password):
+        """
+        Authenticate entity's legitimacy
+
+        param: user_name: the name of the user
+        param: user_password: the password of the user
+
+        return: entity_id: the entity to which the user belongs
+        in case of success, or otherwise raise an exception
+        """
+
+    @abstractmethod
+    def get_status(self, entity_id):
+        """
+        Return the status of an already authenticated entity
 
         param: entity_id: the id of the enetity
         return: an integer indicating active, disabled, inactive, etc.
@@ -16,8 +29,10 @@ class Authentication:
 
     @abstractmethod
     def is_admin(self, entity_id):
-        """Verify admin's legitimacy
+        """
+        Authenticate admin's legitimacy, more privillege is accessible
+        to admin as compared to non-admin users
 
         param: entity_id: the id of the enetity
-        return: success or failure
+        return: None in case of success, or otherwise raise an exception
         """
