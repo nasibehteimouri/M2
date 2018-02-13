8f051e3d317959d11acdc03f168ede510052b836
nasibehteimouri <nasibehteimouri@gmail.com>
2018-02-13 18:48:35 -0500

    pep8 problem solved + class name changed

--- m2/interface/target.py
+++ m2/interface/target.py
@@ -1,8 +1,8 @@
-""" Target Interface """
+""" Diskless Interface """
 from abc import ABCMeta, abstractmethod
 
 
-class Target:
+class Diskless:
     """ Specify provisioning target """
     __metaclass__ = ABCMeta
 
@@ -10,23 +10,23 @@ class Target:
     def add_target(self, instance_id, image_id):
         """
         Create a provisioning target to provision an instance using an image
-	param: instance_id: the id of the provisioned instance
-        param: image_id: the id of the image 
-        return: target_id: the id of the provisioning target 
+        param: instance_id: the id of the provisioned instance
+        param: image_id: the id of the image
+        return: target_id: the id of the provisioning target
         """
 
     @abstractmethod
     def delete_target(self, instance_id):
         """
         Remove a target
-	param: instance_id: the id of the provisioned instance 
-        return: none in case of success, or otherwise raise an exception 
+        param: instance_id: the id of the provisioned instance
+        return: none in case of success, or otherwise raise an exception
         """
 
     @abstractmethod
     def get_target_id(self, instance_id):
         """
         Get the provisioning target id used for provisioning
-	param: instance_id: the id of the provisioned instance
-        return: target_id: the id of the provisioning target 
+        param: instance_id: the id of the provisioned instance
+        return: target_id: the id of the provisioning target
         """
