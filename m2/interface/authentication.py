708a3bc53eeb8ba775c146fbdc68663b2a1284fe
nasibehteimouri <nasibehteimouri@gmail.com>
2018-02-12 21:34:14 -0500

    comments are added

--- m2/interface/authentication.py
+++ m2/interface/authentication.py
@@ -1,15 +1,23 @@
-"""Interface for Authentication"""
+"""Authentication Interface"""
 from abc import ABCMeta, abstractmethod
 
 
 class Authentication:
-    """Class for confirming the legitimacy of a user"""
+    """Confirm the legitimacy of a user"""
     __metaclass__ = ABCMeta
 
     @abstractmethod
-    def is_admin(self):
-        """Verify admin's authority"""
+    def is_legit(self, entity_id):
+        """Verify entity's legitimacy
+
+        param: entity_id: the id of the enetity
+        return: an integer indicating active, disabled, inactive, etc.
+        """
 
     @abstractmethod
-    def is_legit(self):
-        """verify user's authority"""
+    def is_admin(self, entity_id):
+        """Verify admin's legitimacy
+
+        param: entity_id: the id of the enetity
+        return: success or failure
+        """
