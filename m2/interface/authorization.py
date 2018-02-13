c079459a8a10590476d5636e1f375ab0d0d02193
nasibehteimouri <nasibehteimouri@gmail.com>
2018-02-12 20:54:04 -0500

    comments + some more methods are added.

--- m2/interface/authorization.py
+++ m2/interface/authorization.py
@@ -1,19 +1,56 @@
-"""Interface for Authorization"""
+"""Auhtorization Interface"""
 from abc import ABCMeta, abstractmethod
 
 
 class Authorization:
-    """Class for specifying access rights/privileges to resources"""
+    """Specify access rights/privileges to resources"""
     __metaclass__ = ABCMeta
 
     @abstractmethod
-    def authorize_access_node(self, node_id):
-        """Authorize entity's access to a specific node"""
+    def check_entity_node_access(self, entity_id, node_mac_addres):
+        """Authorize entity's access to reserve a specific node
 
-    @abstractmethod
-    def authorize_access_image(self, image_id):
-        """Authorize entity's access to a specific image"""
+        param: entity_id: the id of the enetity
+        param: node_mac_addres: the mac address of the node
+        return: success or failure
+        """
 
-    @abstractmethod
-    def authorize_access_tag(self, tag_id):
-        """Authorize entity's access to a specific tag"""
+    def check_entity_instance_access(self, entity_id, instance_id):
+        """Authorize entity's access to a provisioned instance
+
+        param: entity_id: the id of the enetity
+        param: instance_id: the id of the provisioned instance
+        return: success or failure
+        """
+
+    def check_entity_storage_access(self, entity_id, storage_id):
+        """Authorize entity's access to read/write from/to a storage
+
+        param: entity_id: the id of the enetity
+        param: storage_id: the id of the storage
+        return: success or failure
+        """
+
+    def check_entity_image_access(self, entity_id, instance_id):
+        """Authorize entity's access to an image
+
+        param: entity_id: the id of the enetity
+        param: image_id: the id of the image
+        return: success or failure
+        """
+
+    def check_entity_tag_access(self, entity_id, tag_id):
+        """Authorize entity's access to a tag
+
+        param: entity_id: the id of the enetity
+        param: tag_id: the id of the tag
+        return: success or failure
+        """
+
+    def check_entity_network_access(self, entity_id, network_id):
+        """Authorize entity's access to a network
+
+        param: entity_id: the id of the enetity
+        param: network_id: the id of the tag
+        return: success or failure
+        """
