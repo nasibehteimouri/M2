8c1bc46cccb98f26ed3a904a165b4190d6631ac7
nasibehteimouri <nasibehteimouri@gmail.com>
2018-02-22 14:10:39 -0500

    extra meethod is removed

--- m2/interface/authorization.py
+++ m2/interface/authorization.py
@@ -16,16 +16,6 @@ class Authorization:
         return: None in case of success, or otherwise raise an exception
         """
 
-    def check_entity_storage_access(self, entity_id, rbd_name):
-        """
-        Authorize entity's access to read/write from/to a
-        specific storage.
-
-        param: entity_id: the id of the enetity
-        param: rbd_name: the name of the storage
-        return: None in case of success, or otherwise raise an exception
-        """
-
     def check_entity_network_access(self, entity_id, network_id):
         """
         Authorize entity's access to a specific NIC
